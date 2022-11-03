e9cd40f59e414b231fdd17a91e8d536e50876c2a
jhnam2250 <rs225@naver.com>
2022-11-03 17:27:21 +0900

    retest

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import Spline from "@splinetool/react-spline";
 
 export default function App() {
   return (
-    <div style={{ height: "100vw" }}>
-      <Spline scene="https://prod.spline.design/6MvzYDm9vAIKrX09/scene.splinecode" />
-    </div>
+    <Spline scene="https://prod.spline.design/qkyXtvnuDh6rFo47/scene.splinecode" />
   );
 }
