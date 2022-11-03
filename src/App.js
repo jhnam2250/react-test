23cfba1292a865f06a124d9d0586dfdd42dcf5a6
jhnam2250 <rs225@naver.com>
2022-11-03 17:51:58 +0900

    reset img

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import Spline from "@splinetool/react-spline";
 
 export default function App() {
   return (
-    <div style={{ height: "500vw" }}>
-      <Spline scene="https://prod.spline.design/6MvzYDm9vAIKrX09/scene.splinecode" />
+    <div style={{ height: "1000vw" }}>
+      <Spline scene="https://prod.spline.design/qkyXtvnuDh6rFo47/scene.splinecode" />
     </div>
   );
 }
