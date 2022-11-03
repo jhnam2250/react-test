b58a600b86fa47cf45797152dd16a457bc4083bf
jhnam2250 <rs225@naver.com>
2022-11-03 17:56:02 +0900

    height edit 100

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Spline from "@splinetool/react-spline";
 
 export default function App() {
   return (
-    <div style={{ height: "500vw" }}>
+    <div style={{ height: "100vw" }}>
       <Spline scene="https://prod.spline.design/qkyXtvnuDh6rFo47/scene.splinecode" />
     </div>
   );
